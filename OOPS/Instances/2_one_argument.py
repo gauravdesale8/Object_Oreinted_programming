9c1890dd183c23316b89c2194d4ee92d7117b10f
Gaurav  Desale <45401708+gauravdesale8@users.noreply.github.com>
2023-07-10 19:25:42 +0530

    Add files via upload

---
+++ OOPS/Instances/2_one_argument.py
@@ -0,0 +1,8 @@
+class Circle:
+
+    def __init__(self, radius):
+        self.radius = radius
+
+my_circle = Circle(5)
+
+print(my_circle)
---
+++ OOPS/Instances/3_multiple_arguments.py
@@ -0,0 +1,9 @@
+class Rectangle:
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+my_rectangle = Rectangle(2, 5)
+
+print(my_rectangle)
